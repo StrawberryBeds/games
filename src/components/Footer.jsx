767e40b5223aa3562522bd79cdff1191a7adb148
StrawberryBeds <samuel.l.wood@gmail.com>
2025-06-16 05:51:03 -0400

    Card image credit.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     
     return(
         <footer className="footer">
-            <p>Copyright 2025 Samuel Wood</p>
+            <p>Copyright 2025 Samuel Wood. Card images by  <a href="https://openmoji.org">OpenMoji</a></p>
             <p>This is a work in progress. Use at your own risk.</p>
             <p>If you're not Gracie, you probably shouldn't be here.</p>
         </footer>
