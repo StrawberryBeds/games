71022a438a41260a3cc6e31ecd1deff6b430c7e1
StrawberryBeds <samuel.l.wood@gmail.com>
2025-06-16 01:51:12 -0400

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import './header.css'
+
+function Header () {
+
+    return (
+        <header className="header">
+            <h2>Sam's Company</h2>
+            <h2>Kim's Game</h2>
+            <h2>My Account</h2>
+        </header>
+    )
+}
+
+export default Header;
+
