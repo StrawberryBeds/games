1c8f1a3681e00c01f535b6fff48540924a1763dc
StrawberryBeds <samuel.l.wood@gmail.com>
2025-06-16 04:44:56 -0400

    Change for Gracie's birthday

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ function Header () {
 
     return (
         <header className="header">
-            <h2>Sam's Company</h2>
-            <h2>Kim's Game</h2>
-            <h2>My Account</h2>
+            <h2>!!!!</h2>
+            <h2>Happy Birthday Gracie !</h2>
+            <h2>!!!!</h2>
         </header>
     )
 }
