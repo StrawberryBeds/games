160cf77a6f33fcb75636815c8e9d5fcbd76344f6
StrawberryBeds <samuel.l.wood@gmail.com>
2025-06-30 18:28:58 -0400

    Changes titre de la page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header () {
     return (
         <header className="header">
             <h2>!!!!</h2>
-            <h2>Happy Birthday Gracie !</h2>
+            <h2>Happy Birthday Felix !</h2>
             <h2>!!!!</h2>
         </header>
     )
